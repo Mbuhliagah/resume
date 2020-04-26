eb0380eb3e8787abf23e81487691a6944c991130
MBuhliagah <mbuhliagah@gmail.com>
2020-04-26 08:13:50 +0300

    Updates

---
+++ precache-manifest.5f51001d5c686f71961205b4e854a727.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87f914f9d654fa7fe90810e590bd666",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "9b5faaec8ae2d9cf92a9",
+    "url": "/resume/static/css/main.0379a0df.chunk.css"
+  },
+  {
+    "revision": "1a147760367cb73e6f7e",
+    "url": "/resume/static/js/2.b3ff5caf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.b3ff5caf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5faaec8ae2d9cf92a9",
+    "url": "/resume/static/js/main.eb137b35.chunk.js"
+  },
+  {
+    "revision": "bcaa68c0b5f7adc089f7",
+    "url": "/resume/static/js/runtime-main.05fbbc33.js"
+  },
+  {
+    "revision": "4065b2c4368585abd96b8f7978904ef5",
+    "url": "/resume/static/media/header1.4065b2c4.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/resume/static/media/map-image.f40ed5b1.png"
+  }
+]);
